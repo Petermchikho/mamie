9425c06b6e166d9fe3c56e9e91b68c3bbb7250ff
peter mchikho <petercharlesmchikho1@gmail.com>
2024-02-18 00:17:17 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+  function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    
+    heart.innerText = '💗';
+    
+    document.body.appendChild(heart);
+    
+    setTimeout(() => {
+                heart.remove();
+                }, 5000);
+  }
+  
+  setInterval(createHeart, 300);
